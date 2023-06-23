120c025e837ecad511c2ce1ce1381e09b01132dd
EnriquezAlberto <betooenriquez6234@gmail.com>
2023-06-23 02:03:36 -0300

    cambios js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const boton = document.querySelector('button');
 const color = document.getElementById('color');
 
-function generarColor(){
-let digitos = '0123456789ABCDEFGH';
+function generarColor() {
+  let digitos = '0123456789ABCDEF';
+  let colorHex = '#';
 
+  for (let i = 0; i < 6; i++) {
+    let indiceAleatorio = Math.floor(Math.random() * 16);
+    colorHex += digitos[indiceAleatorio];
+  }
+  
+  return colorHex;
 }
+
+boton.addEventListener('click', function() {
+  let colorAleatorio = generarColor();
+  color.textContent = colorAleatorio;
+  document.body.style.backgroundColor = colorAleatorio;
+});
+
